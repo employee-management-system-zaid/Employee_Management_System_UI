24bc4fa0471b2a503c0d8fdf5b31b76c50e0a5e8
Zaid Alam <zaidalam0731@gmail.com>
2024-05-01 02:22:10 -0400

    Added Connection with backend and Login sgnup code

---
+++ src/components/Login/Login.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import styles from "./Login.module.css";
+
+function Login() {
+  const path = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!email) {
+      errors.email = "Email is required.";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      errors.email = "Invalid email address.";
+    }
+
+    if (!password) {
+      errors.password = "Password is required.";
+    }
+
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  async function submit(e) {
+    e.preventDefault();
+
+    if (validateForm()) {
+      const userCredentials = {
+        email,
+        password,
+      };
+
+      try {
+        const response = await axios.post(`/login`, userCredentials);
+        if (response.data.message === "Incorrect password") {
+          alert("Invalid Credentials");
+        } else if (response.data.message === "User not found") {
+          alert("User does not exist");
+        } else if (response.data.userData) {
+          path("/");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        alert("Something went wrong. Please try again.");
+      }
+    }
+  }
+
+  return (
+    <div className="d-flex mt-4 justify-content-center align-items-center">
+      <div id={styles.mainContainer}>
+        <form onSubmit={submit} id={`login`}>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            id="email"
+            value={email}
+          />
+          {formErrors.email && (
+            <p className={styles.errorMsg}>{formErrors.email}</p>
+          )}
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            value={password}
+          />
+          {formErrors.password && (
+            <p className={styles.errorMsg}>{formErrors.password}</p>
+          )}
+          <input type="submit" value="Login" />
+        </form>
+        <div id={styles.signLink}>
+          <Link to="/signup">Don't have an Account. Click here to Signup </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import styles from "./Signup.module.css";
+
+function Signup() {
+  const path = useNavigate();
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!fullName) {
+      errors.fullName = "Full Name is required";
+    } else if (!/^[^-\s\d][a-zA-Z\s-]+$/.test(fullName)) {
+      errors.fullName = "FullName cannot be numeric";
+    }
+
+    if (!email) {
+      errors.email = "Email is required.";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      errors.email = "Invalid email address.";
+    }
+
+    if (!password) {
+      errors.password = "Password is required.";
+    }
+
+    if (!phoneNumber) {
+      errors.phoneNumber = "Phone number is required.";
+    } else if (!/^\d{3}\d{4}\d{3}$/.test(phoneNumber)) {
+      errors.phoneNumber = "Phone number must be in the format XXXXXXXXXX.";
+    }
+
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const clearData = () => {
+    setFullName("");
+    setEmail("");
+    setPassword("");
+    setPhoneNumber("");
+    // document.getElementById("signUp").reset();
+  };
+
+  async function submit(e) {
+    e.preventDefault();
+
+    if (validateForm()) {
+      const userData = {
+        fullName,
+        email,
+        password,
+        phoneNumber,
+      };
+
+      try {
+        const response = await axios.post("/signup", userData);
+
+        if (response.data === "User Exists") {
+          clearData();
+          alert("User already Exits");
+        } else if (response.data === "User Does Not Exist") {
+          path("/login");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        alert("Something went wrong. Please try again.");
+      }
+    }
+  }
+
+  return (
+    <div id={styles.mainDiv}>
+      <div id={styles.mainContainer}>
+        <h1>Sign Up</h1>
+        <form onSubmit={submit} id={`signUp`}>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            onChange={(e) => setFullName(e.target.value)}
+            name="fullName"
+            id="fullName"
+            value={fullName}
+          />
+          {formErrors.fullName && (
+            <p className={styles.errorMsg}>{formErrors.fullName}</p>
+          )}
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            id="email"
+            value={email}
+          />
+          {formErrors.email && (
+            <p className={styles.errorMsg}>{formErrors.email}</p>
+          )}
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input
+            type="number"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            name="phoneNumber"
+            id="phoneNumber"
+            value={phoneNumber}
+          />
+          {formErrors.phoneNumber && (
+            <p className={styles.errorMsg}>{formErrors.phoneNumber}</p>
+          )}
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            value={password}
+          />
+          {formErrors.password && (
+            <p className={styles.errorMsg}>{formErrors.password}</p>
+          )}
+          <input type="submit" value="Signup" />
+        </form>
+        <div id={styles.loginLink}>
+          <Link to="/login">Already have an Account. Click here to Login </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
